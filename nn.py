887b95e00abe7a1f80a8243167ed1fa962c37969
marcshepard <marcshepard@outlook.com>
2023-02-22 16:11:43 -0800

    Fixed bias adjustments and tests. Multilayer nn is now working, sans activation functions. Will go after them next.

--- nn.py
+++ nn.py
@@ -8,16 +8,15 @@ since x = input, y = output, w = weight, b = bias, err = error to be backprop'ed
 gradient, dB = bias gradient, nn = neural net, are all common variable names in neural nets, so
 artificially forcing longer names would be silly.
 """
-import random
 from typing import Tuple
 import numpy as np
 
 # BackpropOutput is a tuple of (backprop_error, dW, db)
 BackpropOutput = Tuple[np.ndarray, np.ndarray, np.ndarray]
 
-debug_trace = False
 def trace (*args):
-    if debug_trace:
+    """Print out the arguments if debug tracing is enabled"""
+    if False:           # pylint: disable=using-constant-test
         print (*args)
 
 class Layer:
@@ -29,9 +28,9 @@ class Layer:
 
     def backward(self, x: np.ndarray, err: np.ndarray) -> BackpropOutput:  # pylint: disable=unused-argument
         """Backward pass of the layer, given x_input and backprop'ed output error err
-        Returns: (back_err, dW, db) where back_err is the error that should be backproped to the
-        previous layer, dW is the gradient of the weights, and db is the gradient of the bias"""
-        return None, None, None    # Only returning these to make pylint happy
+        Returns: (back_err, dw, db) where back_err is the error that should be backproped to the
+        previous layer, dw is the gradient of the weights, and db is the gradient of the bias"""
+        return None, None, None
 
 
 class Sigmoid (Layer):
@@ -62,21 +61,21 @@ class Linear (Layer):
     """Linear layer"""
 
     def __init__(self, num_inputs: int, num_outputs: int):
-        """Initialize weights and bias of the linear layer"""
+        """Initialize weights and bias with random values"""
         self.w = np.random.randn(num_outputs, num_inputs)
         self.b = np.random.randn(num_outputs, 1)
 
     def forward(self, x: np.ndarray) -> np.ndarray:
-        """Forward pass of the linear layer"""
-        return np.matmul(self.w, x) #+ self.b
+        """Forward pass"""
+        return np.matmul(self.w, x) + self.b
 
     def backward(self, x: np.ndarray, err: np.ndarray) -> BackpropOutput:
-        """Backward pass of the linear layer"""
+        """Backward pass"""
         num_records = x.shape[1]
-        dW = np.matmul(err, x.T) / num_records
-        db = np.sum(err, axis=1, keepdims=True) / num_records
+        dw = np.matmul(err, x.T) / num_records
+        db = np.sum (err, axis = 1, keepdims=True) / num_records
         backprop_error = np.matmul(self.w.T, err)
-        return backprop_error, dW, db
+        return backprop_error, dw, db
 
 class NeuralNetwork:
     """Neural network class"""
@@ -84,19 +83,18 @@ class NeuralNetwork:
     def __init__(self, seed = 99):
         self.layers = []
         self.inputs = []
-        self.errs = []
-        self.dWs = []
-        self.dBs = []
+        self.dws = []
+        self.dbs = []
         self.training_log = []
+        self.learning_rate = 0.05
         np.random.seed(seed)
 
     def add_layer(self, layer: Layer):
         """Add a layer to the neural network"""
         self.layers.append(layer)
         self.inputs.append(None)
-        self.errs.append(None)
-        self.dWs.append(None)
-        self.dBs.append(None)
+        self.dws.append(None)
+        self.dbs.append(None)
 
     def forward(self, x_input: np.ndarray) -> np.ndarray:
         """Forward pass of the neural network"""
@@ -108,12 +106,11 @@ class NeuralNetwork:
     def backward(self, err: np.ndarray):
         """Backward pass of the neural network"""
         for i in range(len(self.layers) - 1, -1, -1):
-            self.errs[i] = err
-            err, self.dWs[i], self.dBs[i] = self.layers[i].backward(self.inputs[i], err)
-            trace ("backprop dW", self.dWs[i], "dB", self.dBs[i])
+            err, self.dws[i], self.dbs[i] = self.layers[i].backward(self.inputs[i], err)
+            trace ("backprop dW", self.dws[i], "dB", self.dbs[i])
 
     def train(self, x_train: np.ndarray, y_train: np.ndarray, epochs: int,
-              learning_rate: float, batch_size: int):
+              batch_size : int= 0):
         """Train the neural network"""
         self.training_log = []
 
@@ -121,7 +118,7 @@ class NeuralNetwork:
         y_train = np.array(y_train)
 
         if batch_size == 0:
-            batch_size = len(x_train)
+            batch_size = x_train.shape[1]
 
         for i in range(epochs):
             for j in range(0, len(x_train), batch_size):
@@ -133,11 +130,11 @@ class NeuralNetwork:
                 self.training_log.append (f"Epoch {i}, Batch {j}, Loss {loss}")
                 trace ("Before backprop, weights are", self.layers[0].w, self.layers[0].b)
                 self.backward(err)
-                self.update_weights(learning_rate)
+                self.update_weights(self.learning_rate)
                 trace ("After backprop, weights are", self.layers[0].w, self.layers[0].b)
                 y_hat = self.forward(x_batch)
                 new_loss = self.loss(y_batch, y_hat)
-                last_learning_rate = learning_rate
+                last_learning_rate = self.learning_rate
                 while new_loss > loss:
                     self.update_weights(-last_learning_rate)
                     last_learning_rate /= 2
@@ -154,48 +151,45 @@ class NeuralNetwork:
         """Update the weights of the neural network"""
         for i, layer in enumerate(self.layers):
             if isinstance(layer, Linear):
-                layer.w -= learning_rate * self.dWs[i]
-                layer.b -= learning_rate * self.dBs[i]
+                layer.w -= learning_rate * self.dws[i]
+                layer.b -= learning_rate * self.dbs[i]
 
     def predict(self, x_input: np.ndarray) -> np.ndarray:
         """Predict the output of the neural network"""
         return self.forward(x_input)
 
 
-def test_1_neuron_identity():
-    """1 layer identity (1 input, 1 output)"""
-    test_name = "1 neuron, 1 variable identity"
+def test_1_variable_identity():
+    """1 variable identity (1 input, 1 output)"""
+    test_name = "1 neuron, 1 input identity"
     nn = NeuralNetwork()
     nn.add_layer(Linear(1, 1))
     x = np.random.randint(20, size=(1, 10))
     y = x
-
-    nn.train(x, y, 100, 0.05, 10)
+    nn.train(x, y, 100)
     x = [92]
     y = [92]
     y_hat = nn.predict(x)
     pct_error = abs((y - y_hat)/y)
     if pct_error > .1:
-        print(test_name, "with input = ", x, "expected output = ", y, \
-                "actual output = ", y_hat)
+        print(test_name, "with input =", x, ", expected output =", y, \
+                ", actual output =", y_hat)
+        print ("Weight/bias =", nn.layers[0].w, nn.layers[0].b)
         print("Test failed: error percent = ", pct_error, ">.1")
     else:
         print (test_name, "passed")
 
-def test_1_neuron_sum():
+def test_1_layer_sum():
     """1 layer sum (2 inputs, 1 output)"""
-    test_name = "1 neuron sum"
+    test_name = "1 neuron, 2 input sum"
     nn = NeuralNetwork()
     nn.add_layer(Linear(2, 1))
     x = np.random.randint(20, size=(2, 10))
     y = x.sum(axis=0, keepdims=True)
-    print (x)
-    print (y)
-
-    global debug_trace
-    debug_trace = True
-    nn.train(x, y, 100, 0.05, 10)
-    x = [92, 17]
+    nn.train(x, y, 100)
+    x = np.ndarray((2, 1))
+    x[0][0] = 92
+    x[1][0] = 17
     y = [109]
     y_hat = nn.predict(x)
     pct_error = abs((y - y_hat)/y)
@@ -205,50 +199,31 @@ def test_1_neuron_sum():
         print("Test failed: error percent = ", pct_error, ">.1")
     else:
         print (test_name, "passed")
-    print(test_name, "with input = ", x, "expected output = ", y, \
-                "actual output = ", y_hat)
-        
 
-def test():
-    #test_1_neuron_identity()
-    test_1_neuron_sum()
-
-    """Test the neural network
-
-    # 1 layer "add" (2 inputs, 1 output)
-    nn = NeuralNetwork()
-    nn.add_layer(Linear(2, 1))
-    x = np.random.randint(20, size=(2, 10))
-    y = np.sum(x, axis=0, keepdims=True)
-    print (x)
-    print (y)
-
-    nn.train(x, y, 100, 0.05, 0)
-    x = [92, 17]
-    y = 109
-    y_hat = nn.predict(x)
-    pct_error = abs(y - y_hat)/y
-    print("1 neuron sum test with input = ", x, "expected output = ", y, \
-              "actual output = ", y_hat)
-    if pct_error > .1:
-        print("Test failed: error percent = ", pct_error, ">.1")
-        print (nn.training_log)
-    
-    # 2d identity test with 2 layer
+def test_2_layer_2_variable_identity():
+    """2d identity test with 2 layer"""
+    test_name = "2 layer, 2 input identity"
     nn = NeuralNetwork()
     nn.add_layer(Linear(2, 3))
     nn.add_layer(Linear(3, 2))
     x = np.random.randint(20, size=(2, 10))
     y = np.copy(x)
-    nn.train(x, y, 100, 0.005, 1)
-    x = [92, -17]
-    y = np.copy(x)
+    nn.train(x, y, 100)
+    x = np.ndarray((2, 1))
+    x[0][0] = 92
+    x[1][0] = -17
+    y = x
     y_hat = nn.predict(x)
-    print("1 neuron sum test with input = ", x, "expected output = ", y, \
-              "actual output = ", y_hat)
-    """
-    """
-    # 2d identity with 1 layer of 2 neurons and ReLU activation
+    pct_error = nn.loss(y_hat, y)/np.sum(np.square(y))
+    if pct_error > .1:
+        print(test_name, "with input = ", x, "expected output = ", y, \
+                "actual output = ", y_hat)
+        print("Test failed: error percent = ", pct_error, ">.1")
+    else:
+        print (test_name, "passed")
+
+def test_activation():
+    """ 2d identity with 1 layer of 2 neurons and ReLU activation"""
     nn = NeuralNetwork()
     nn.add_layer(Linear(2, 2))
     nn.add_layer(ReLU())
@@ -271,7 +246,12 @@ def test():
         print("2 layer 2d identity test w ReLU activation failed: input = ", x, "predicted output = ", y, "error = ", np.sum(np.square(x - y)))
         print(nn.layers[0].weights)
         print(nn.training_log)
-    """
+
+def test():
+    """Test the neural network"""
+    test_1_variable_identity()
+    test_1_layer_sum()
+    test_2_layer_2_variable_identity()
 
 test()
 
