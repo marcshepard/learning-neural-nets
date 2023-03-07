b0881b4f2744e8c326f2eb4197a866e803412876
marcshepard <marcshepard@outlook.com>
2023-03-06 21:24:48 -0800

    Refactored scratchpad to clean up duplicate code and make it easier to add new models.

--- scratchpad.py
+++ scratchpad.py
@@ -10,7 +10,14 @@ so math for linear layer l(x) = x * w.T + b (vs neural_net.py's l(x) = w * x + b
 It's weight initialization is rand(-sqrt(k), sqrt(k)) where k = 1/# inputs
 """
 
+import random
+import os
+import matplotlib.pyplot as plt
 import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
 
 # Configure device to use for tensors (CPU or GPU).
 # If your PC has a GPU, you'll need to perform these steps to allow PyTorch to use it:
@@ -24,131 +31,62 @@ if device == "cuda":
     print(f'Device Name: {torch.cuda.get_device_name()}')
 
 # From https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-
 # Define model
 class NeuralNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, model : torch.nn.Module, loss_fn : torch.nn.modules.loss._Loss):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
-        )
-        # Looks like nn.Module's __str__ method enumerates all fields of type nn.Module
-        self.hello = "hello"
-        self.goodbye = nn.Flatten()
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+        self.loss_per_epoch = []
+        self.trace_debug = True
+        self.to(device)
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+        logits = self.model(x)
         return logits
 
-def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
+    def train(self, train : DataLoader, test : DataLoader, epochs : int):
+        self.loss_per_epoch = []
 
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
+        for e in range(epochs):
+            self.model.train()
+            for X, y in train:
+                X, y = X.to(device), y.to(device)
 
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+                # Compute prediction error
+                pred = self.model(X)
+                loss = self.loss_fn(pred, y)
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+                # Backpropagation
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
 
-def test(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+            loss, accuracy = self.test(test)
+            if self.trace_debug:
+                print (f"Epoch: {e}, Loss: {loss}, Accuracy: {accuracy}%")
+            self.loss_per_epoch.append(loss)
 
-def train_FashionMNIST_model(model_path, model = None, epochs = 5):
-    # Download training data from open datasets.
-    training_data = datasets.FashionMNIST(
-        root="data",
-        train=True,
-        download=True,
-        transform=ToTensor(),
-    )
+    def test (self, test : DataLoader):
+        self.model.eval()
+        loss = 0
+        correct = 0
+        with torch.no_grad():
+            for X, y in test:
+                X, y = X.to(device), y.to(device)
+                pred = self.model(X)
+                loss += self.loss_fn(pred, y).item()
+                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+        return loss / len(test), correct * 100 / len(test.dataset)
+    
+    def predict (self, x : torch.Tensor):
+        self.model.eval()
+        with torch.no_grad():
+            return self.model(x)
 
-    # Download test data from open datasets.
-    test_data = datasets.FashionMNIST(
-        root="data",
-        train=False,
-        download=True,
-        transform=ToTensor(),
-    )
-
-    batch_size = 64
-
-    # Create data loaders.
-    train_dataloader = DataLoader(training_data, batch_size=batch_size)
-    test_dataloader = DataLoader(test_data, batch_size=batch_size)
-
-    for X, y in test_dataloader:
-        print(f"Shape of X [N, C, H, W]: {X.shape}")
-        print(f"Shape of y: {y.shape} {y.dtype}")
-        break
-
-    if model is None:
-        model = NeuralNetwork().to(device)
-        print(model)
-
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
-    for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
-        train(train_dataloader, model, loss_fn, optimizer)
-        test(test_dataloader, model, loss_fn)
-    print("Done!")
-
-    torch.save(model.state_dict(), model_path)
-    print("Saved PyTorch Model State", model_path)
-
-import random
-import matplotlib.pyplot as plt
-def show_FashionMNIST_image(img, label):
-    figure = plt.figure(figsize=(8, 8))
-    plt.title(label)
-    plt.axis("off")
-    plt.imshow(img.squeeze(), cmap="gray")
-    plt.show(block = True)
-
-def evaluate_FashionMNIST_model(path):
-    model = NeuralNetwork()
-    model.load_state_dict(torch.load(path))
-
-    # Download test data from open datasets.
-    test_data = datasets.FashionMNIST(
-        root="data",
-        train=False,
-        download=True,
-        transform=ToTensor(),
-    )
-    classes = [
+class FashionMNIST(NeuralNetwork):
+    CLASSES = [
         "T-shirt/top",
         "Trouser",
         "Pullover",
@@ -161,38 +99,108 @@ def evaluate_FashionMNIST_model(path):
         "Ankle boot",
     ]
 
-    model.eval()
+    def __init__(self):
+        super().__init__(nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        ), nn.CrossEntropyLoss())
+        self.SAVE_DIR = "models"
+        self.SAVE_FILE = os.path.join(self.SAVE_DIR, "fashion_mnist_nn.pth")
 
-    print(f"Model structure: {model}\n\n")
-    #for name, param in model.named_parameters():
-    #    print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
+    @staticmethod
+    def get_test_dataloader ():
+        # Download test data from open datasets.
+        test_data = datasets.FashionMNIST(
+            root="data",
+            train=False,
+            download=True,
+            transform=ToTensor(),
+        )
+        return DataLoader(test_data, batch_size=len(test_data))
+    
+    @staticmethod
+    def get_train_dataloader():
+        # Download training data from open datasets.
+        training_data = datasets.FashionMNIST(
+            root="data",
+            train=True,
+            download=True,
+            transform=ToTensor(),
+        )
+        return DataLoader(training_data, batch_size=64)
 
-    rand_ix = random.randint(0, len(test_data))
-    x, y = test_data[rand_ix][0], test_data[rand_ix][1]
-    with torch.no_grad():
-        pred = model(x)
-        print(f'For test data at index {rand_ix}, Predicted: "{classes[pred.argmax()]}", Actual: "{classes[y]}"')
-        show_FashionMNIST_image(x, classes[y])
+    def save(self):
+        if not os.path.exists(self.SAVE_DIR):
+            os.makedirs(self.SAVE_DIR)
+        torch.save(self.state_dict(), self.SAVE_FILE)
 
-    loss_fn = nn.CrossEntropyLoss()
-    model = model.to(device)
-    test(DataLoader(test_data, batch_size=len(test_data)), model, loss_fn)
+    def load(self) -> bool:
+        try:
+            self.load_state_dict(torch.load(self.SAVE_FILE))
+            return True
+        except:
+            return False
 
-import os
-# Create and train the model if it doesn't exist
-model_dir = "models"
-model_file = os.path.join(model_dir, "fashion_mnist_model.pth")
-if not os.path.exists(model_dir):
-    os.makedirs(model_dir)
-if not os.path.exists(model_file):
-    train_FashionMNIST_model(model_file)
+    def train(self, epochs: int):
+        return super().train(self.get_train_dataloader(), self.get_test_dataloader(), epochs)
 
-#  Optional; more training
-def more_training(epochs):
-    model = NeuralNetwork().to(device)
-    model.load_state_dict(torch.load(model_file))
-    train_FashionMNIST_model(model_file, model, epochs)
-#more_training(20)
+    def show_image(self, img, label):
+        plt.figure(figsize=(8, 8))
+        plt.title(label)
+        plt.axis("off")
+        plt.imshow(img.squeeze(), cmap="gray")
+        plt.show()
 
-evaluate_FashionMNIST_model(model_file)
+    def eval(self):
+        print ("Evaluations the current FashionMNIST model against the test dataset.")
+        test_data = self.get_test_dataloader()
+        loss, accuracy = self.test(test_data)
+        print (f"Loss: {loss}, Accuracy = {accuracy}%")
+        if len(self.loss_per_epoch) > 0:
+            self.plot_loss()
 
+        test_images, test_labels = next(iter(test_data))
+        while input("Type enter to show a prediction from a random index, q to quit: ") != "q":
+            rand_ix = random.randint(0, len(test_images))
+            x = test_images[rand_ix]
+            y = test_labels[rand_ix]
+            with torch.no_grad():
+                pred = self(x.to(device))
+                print(f'For test data at index {rand_ix}, Predicted: "{self.CLASSES[pred.argmax()]}", Actual: "{self.CLASSES[y]}"')
+                self.show_image(x, self.CLASSES[y])
+
+    def plot_loss(self):
+        """Show loss per epoch"""
+        plt.plot(self.loss_per_epoch)
+        plt.title("Loss per epoch", loc = "left")
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        plt.show()
+
+    def print_model(self):
+        print(f"Model structure: {self}\n\n")
+        for name, param in self.named_parameters():
+            print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
+
+nn = FashionMNIST()
+
+# Try to load existing model, or create from scratch if a model wasn't saved
+if not nn.load():
+    epochs = 5
+    print (f"Creating the FashionMNIST model for {epochs} epochs, please be patient...")
+    nn.train(epochs)
+    nn.save()
+
+# Optionally, do a bit more training
+if False:
+    epochs = 50
+    print (f"Training the FashionMNIST model for {epochs} more epochs, please be patient...")
+    nn.train(epochs)
+    nn.save()
+
+# Evaluate the model
+nn.eval()
